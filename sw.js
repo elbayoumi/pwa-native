58e67e92fe7fabb634ae4220e358d4e87e7a2d1e
elbayoumi <mohamedashrafelbayoumi.com>
2023-04-06 10:23:23 +0200

    v0

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'my-pwa-cache';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'assets/css/templatemo-softy-pinko.css',
+  'assets/images/logo.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch Event Listener
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
